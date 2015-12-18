e9f41d22334a13b01f202ce3f060ce239081f689
unknown <moriya-h5@confrage.com>
2015-12-18 17:07:08 +0900

    初期登録

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+
+<title>問い合わせフォーム</title>
+</head>
+<body>
+
+</body>
+</html>
